576b30b10d25045fb1143046133049bef5174eef
kwonkihoon <inyourcare@sotongfive.kr>
2022-12-19 13:27:35 +0900

    og:image serversideProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,25 @@
+import { logger } from '@core/logger';
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
+import Head from 'next/head';
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({ ogImage }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <>
+      <Head>
+        {/* <meta name="og:image" content={`${router.basePath}`} /> */}
+        <meta name="og:image" content={`${ogImage}`} />
+      </Head>
       Home
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { req } = context;
+  logger.debug('index:getServerSideProps', req?.headers.host)
+  const ogImage = `${req?.headers.host}/api/og`
+  return {
+    props: { ogImage }
+  };
 }
