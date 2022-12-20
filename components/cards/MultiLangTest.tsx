1f8aba4608836cd36cf41ed37969d63ceecb8593
kwonkihoon <inyourcare@sotongfive.kr>
2022-12-20 09:23:01 +0900

    다이얼로그 일반화

--- components/cards/MultiLangTest.tsx
+++ components/cards/MultiLangTest.tsx
@@ -1,11 +1,16 @@
+import PrintDialog from "@components/dialogs/print";
 import Link from "@components/wrapper/link";
 import { useRouter } from "next/router";
 import blogPosts from 'public/lang/lang.json';
+import { useState } from "react";
 
-export default function HomeCard() {
+export default function MultiLangTestCard() {
 
     const { locale, locales, asPath } = useRouter();
+    const [printDialogOpen, setPrintDialogOpen] = useState(false)
     return (<div>
+        <PrintDialog open={printDialogOpen} setOpen={setPrintDialogOpen} />
+        <button onClick={()=>setPrintDialogOpen(true)}>dialog open</button>
         <h1>다국어 설정</h1>
         <div>
             {locales?.map((l, i) => {
