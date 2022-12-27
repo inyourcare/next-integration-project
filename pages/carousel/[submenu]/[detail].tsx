4382f70342e9005aeaf887ead30627d7af87d47a
kwonkihoon <inyourcare@sotongfive.kr>
2022-12-27 17:49:45 +0900

    home link 추가

--- pages/carousel/[submenu]/[detail].tsx
+++ pages/carousel/[submenu]/[detail].tsx
@@ -1,10 +1,11 @@
 import CarauselLayout from "@components/layouts/carausel/layout";
+import Link from "@components/wrapper/link";
 import { getCarouselItems } from "@core/staticfiles";
 import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 
 export default function Carousel({ data }: InferGetServerSidePropsType<typeof getServerSideProps>) {
     const { items } = data
-    return <CarauselLayout items={items}><div>carousel</div></CarauselLayout>
+    return <CarauselLayout items={items}><div>carousel<Link href={'/'}>home</Link></div></CarauselLayout>
 }
 interface Props {
     data: {
