5a69531d1188b9e8825b7a25682a3aa2918103f3
Kwon KiHoon <inyourcareallen@gmail.com>
2023-01-17 17:39:18 +0900

    윈도우에서 이름없는 function link 워닝 때문에 수정

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -1,31 +1,32 @@
-import { ImageResponse } from '@vercel/og';
+import { ImageResponse } from "@vercel/og";
+import React from "react";
 
 export const config = {
-    // runtime: 'edge',
-    runtime: "experimental-edge"
+  // runtime: 'edge',
+  runtime: "experimental-edge",
 };
 
 export default function () {
-    return new ImageResponse(
-        (
-            <div
-                style={{
-                    fontSize: 128,
-                    background: 'white',
-                    width: '100%',
-                    height: '100%',
-                    display: 'flex',
-                    textAlign: 'center',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                }}
-            >
-                대구 창조 경제
-            </div>
-        ),
-        {
-            width: 1200,
-            height: 600,
-        },
-    );
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 128,
+          background: "white",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          textAlign: "center",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        대구 창조 경제
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 600,
+    }
+  );
 }
