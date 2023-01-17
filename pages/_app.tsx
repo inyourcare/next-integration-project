a44c6922199dd0b245cbfa74958519ff3e0e65f3
Kwon KiHoon <inyourcareallen@gmail.com>
2023-01-17 17:27:04 +0900

    client side fetch example

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,25 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { SessionProvider } from "next-auth/react"
-import { Hydrate, QueryClient, QueryClientProvider } from 'react-query'
-import { useState } from 'react';
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
+import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
+import { useState } from "react";
+import { ApolloProvider } from "@apollo/client";
+import apollo from "apollo";
 
-export default function App({ Component, pageProps: { session, ...pageProps }, }: AppProps) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
-  return <QueryClientProvider client={queryClient}>
-    <Hydrate state={pageProps.dehydratedState}>
-      <SessionProvider session={session}>
-        <Component {...pageProps} />
-      </SessionProvider>
-    </Hydrate>
-  </QueryClientProvider>
+  return (
+    <ApolloProvider client={apollo}>
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <SessionProvider session={session}>
+            <Component {...pageProps} />
+          </SessionProvider>
+        </Hydrate>
+      </QueryClientProvider>
+    </ApolloProvider>
+  );
 }
