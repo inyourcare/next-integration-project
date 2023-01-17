351bd68b9cded82a7878a01c797b1d0405442751
Kwon KiHoon <inyourcareallen@gmail.com>
2023-01-17 17:21:59 +0900

    getserversideprops

--- pages/graphQl/[submenu]/[detail].tsx
+++ pages/graphQl/[submenu]/[detail].tsx
@@ -2,11 +2,12 @@ import GraphQlCard from "@components/cards/graphql/GraphQlCard";
 import BaseLayout from "@components/layouts/base/layout";
 import { gql } from "@apollo/client";
 import apollo from "apollo";
-import { GetStaticPaths, InferGetStaticPropsType } from "next";
+import { GetStaticPaths, InferGetServerSidePropsType, InferGetStaticPropsType } from "next";
 
 export default function GraphQl({
   countries,
-}: InferGetStaticPropsType<typeof getStaticProps>) {
+// }: InferGetStaticPropsType<typeof getStaticProps>) {
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <BaseLayout>
       <GraphQlCard countries={countries}></GraphQlCard>
@@ -14,7 +15,34 @@ export default function GraphQl({
   );
 }
 
-export async function getStaticProps() {
+// export async function getStaticProps() {
+//   const { data } = await apollo.query({
+//     query: gql`
+//       query Countries {
+//         countries {
+//           code
+//           name
+//           emoji
+//         }
+//       }
+//     `,
+//   });
+
+//   return {
+//     props: {
+//       countries: data.countries.slice(0, 4),
+//     },
+//   };
+// }
+
+// export const getStaticPaths: GetStaticPaths<{ slug: string }> = async () => {
+//   return {
+//     paths: [], //indicates that no page needs be created at build time
+//     fallback: "blocking", //indicates the type of fallback
+//   };
+// };
+
+export async function getServerSideProps() {
   const { data } = await apollo.query({
     query: gql`
       query Countries {
@@ -33,10 +61,3 @@ export async function getStaticProps() {
     },
   };
 }
-
-export const getStaticPaths: GetStaticPaths<{ slug: string }> = async () => {
-  return {
-    paths: [], //indicates that no page needs be created at build time
-    fallback: "blocking", //indicates the type of fallback
-  };
-};
