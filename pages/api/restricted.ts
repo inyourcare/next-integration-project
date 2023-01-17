ef3edf7155b6b454470d31e1cafae587f7324965
Kwon KiHoon <inyourcareallen@gmail.com>
2023-01-17 17:51:11 +0900

    주석추가

--- pages/api/restricted.ts
+++ pages/api/restricted.ts
@@ -1,6 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next"
 import { unstable_getServerSession } from "next-auth/next"
 import { authOptions } from "./auth/[...nextauth]"
+
+// api 권한 관리 예시
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     const session = await unstable_getServerSession(req, res, authOptions)
     if (session) {
